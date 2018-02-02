619ffe8300008b57197842b88eb7dd5b3f977b48
szabolcsit <szabolcsit@hu.ibm.com>
2018-02-02 23:38:02 +0100

    refactor home state

--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -7,26 +7,38 @@ import reduce from '../../reducers';
 import ColorPicker from '../../components/ColorPicker';
 import TimePicker from '../../components/TimePicker';
 
+const mapStateToProps = (state, ownProps) => {
+    const outerColor = state.outerColor
+    const time = state.time
+
+    return {
+        outerColor,
+        time
+    }
+}
+
 @connect(reduce, actions)
-export default class Home extends Component {
+class H extends Component {
 
     setOuterColor (color) {
         this.props.setOuterColor(color)
     }
 
-    setTime (date) {
-
+    setTime (time) {
+        console.log('time', time);
+        this.props.setTime(new Date(time))
     }
 
-    render() {
+    render({outerColor, time}, state) {
+        console.log(outerColor);
         return (
             <div class={style.home}>
-
-                <TimePicker onChange={e => this.setTime(e)} />
-
+                <TimePicker onChange={e => this.setTime(e)} time={time} outerColor={outerColor} />
                 <ColorPicker text="Outer color" onChange={e => this.setOuterColor(e)} />
-
             </div>
         );
     }
 }
+
+const Home = connect(mapStateToProps)(H)
+export default Home
