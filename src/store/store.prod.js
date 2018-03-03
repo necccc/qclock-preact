b8aa281fa79551a7bcbf2f618e138b761a5a33b7
szabolcsit <szabolcsit@hu.ibm.com>
2018-03-03 19:11:50 +0100

    missing socket middleware

--- src/store/store.prod.js
+++ src/store/store.prod.js
@@ -1,14 +1,16 @@
 import { createStore, applyMiddleware } from 'redux'
 import thunkMiddleware from 'redux-thunk'
 import apiMiddleware from '../middleware/api'
+import socketMiddleware from '../middleware/socket'
 import reducers from '../reducers'
 
 const store = createStore(
-    reducers,
-    applyMiddleware(
-        thunkMiddleware,
-        apiMiddleware
-      )
+	reducers,
+	applyMiddleware(
+		thunkMiddleware,
+		socketMiddleware,
+		apiMiddleware
+	  )
 );
 
 export default store
